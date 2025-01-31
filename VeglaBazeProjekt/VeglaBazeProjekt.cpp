b3a67b2e613f5502aed3f3040018b2c80257c895
Artrit Telaku <artrittelaku@gmail.com>
2025-01-31 22:46:29 +0100

    Detyrat

--- VeglaBazeProjekt/VeglaBazeProjekt.cpp
+++ VeglaBazeProjekt/VeglaBazeProjekt.cpp
@@ -1,6 +1,9 @@
 /*Projekti ne Github*/
 
-/*Arbeniti Ktuuuuuuu*/
+/*Detyrat per Artrit Telakub: Krijo aplikacionin, funksione dhe rregullo funksionin main*/
+/*Detyrat per Arbenit Krasniq-in: Krijo nje funksion dhe inmplemento ne main*/
+/*Detyrat per Orges Gegaj: Krijo nje funksion dhe inmplemento ne main*/
+
 
 #include <iostream>
 
